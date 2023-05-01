8c2c824f787d36284fd5e7a13e6695b9e975dcaf
vikto <viktorijhohlova@yandex.ru>
2023-05-01 22:57:43 +0700

    Тесты на создание классов Code, Player, Game

--- tests/tests.py
+++ tests/tests.py
@@ -5,5 +5,19 @@ sys.path.append('../')
 from main import *
 
 
+class TestGame(unittest.TestCase):
+	def test_class_Code(self):
+		code = Code(text_code, bags)
+		self.assertIsNotNone(code)
+
+	def test_class_Player(self):
+		player = Player("Tim")
+		self.assertIsNotNone(player)
+
+	def test_class_Game(self):
+		game = Game()
+		self.assertIsNotNone(game)
+
+
 if __name__ == '__main__':
 	unittest.main()
