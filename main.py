be7c2f6532ec0eb1eafc02e74ab4ef7f5da4da18
vikto <viktorijhohlova@yandex.ru>
2023-05-02 02:16:19 +0700

    Создание и добавление метода чтения из файла кода для игры в классе Code

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ class Code(object):
 		self.text_code = text_code
 		self.bugs = bugs
 
+	def get_test_code_text(self):
+		i = 1
+		text_code_file = open("text_code1.txt", "r")
+		print()
+		for line in text_code_file:
+			print(i, line)
+			self.text_code.append(line)
+			i += 1
+		text_code_file.close()
+		print()
+
 
 class Player(object):
 
@@ -21,6 +32,12 @@ class Game(object):
 		self.code = Code()
 		self.player1 = Player()
 		self.player2 = Player()
+		self.bags_list = {1: {2:"    if n <= 1:", 
+					 6:"        for i in range(1, n):", 
+					 7: "            m, k = k, (m+k)%10", 
+					 13: "    print(fib_digit(n))", 
+					 16:'''if __name__ == "__main__":''',
+					 17: "    main()"}}
 
 	def get_activ_player(self):
 		if self.player1.activ:
@@ -51,6 +68,8 @@ class Game(object):
 	def start_game(self):
 		total_account = 0
 		answers = dict()
+		self.code.get_test_code_text()
+		self.code.bags = self.bags_list[1]
 		total_errors = len(self.code.bugs)
 
 		while total_account < total_errors:
