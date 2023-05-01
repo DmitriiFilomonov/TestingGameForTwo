4845d184f63d7f00e440e1477753558f541a767d
vikto <viktorijhohlova@yandex.ru>
2023-05-02 00:54:12 +0700

    Создание метода начала игры, поиска активного игрока в классе Game и метода изменения статуса игрока в классе Player

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ class Player(object):
 		self.score = 0
 		self.activ = True
 
+	def set_activ_status(self, bool_value):
+		self.activ = bool_value
+
 
 class Game(object):
 
@@ -19,9 +22,30 @@ class Game(object):
 		self.player1 = Player()
 		self.player2 = Player()
 
+	def get_activ_player(self):
+		if self.player1.activ:
+			print("Начинает игрок №1", self.player1.name)
+			self.player1.set_activ_status(False)
+			self.player2.set_activ_status(True)
+			return self.player1
+		else:
+			print("Начинает игрок №2", self.player2.name)
+			self.player1.set_activ_status(True)
+			self.player2.set_activ_status(False)
+			return self.player2
+
+	def start_game(self):
+		total_account = 0
+		answers = dict()
+		total_errors = len(self.code.bugs)
+
+		while total_account < total_errors:
+			activ_player = self.get_activ_player()
+			break
+
 
 def main():
-	print("Hello world")
+	Game().start_game()
 
 
 if __name__ == '__main__':
